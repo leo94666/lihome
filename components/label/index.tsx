a6eeb0cf6c8904ac79ece1132770eef22b30b359
leo <719947897@qq.com>
2023-05-19 15:09:36 +0800

    init

--- components/label/index.tsx
+++ components/label/index.tsx
@@ -10,8 +10,8 @@ type Props = {
 export default function Index(props: Props) {
   const { className, title, url } = props;
   return (
-    <div className={`${className} m-4 w-16 h-24 bg-amber-900  items-center`}>
-      <Image className={`${className} rounded-full w-16 h-16 bg-red-900 opacity-90 p-2 hover:p-1 `} src={url} alt="icon" width={240} height={240}/>
+    <div className={`${className} m-4 w-16 h-24  items-center`}>
+      <Image className={`${className} rounded-full w-16 h-16 bg-red-800 opacity-90 p-2 hover:p-1 `} src={url} alt="icon" width={240} height={240}/>
       <p className={"text-white text-center justify-center w-16 h-8 mt-1"}>
         {title}
       </p>
--- components/supertabs/index.tsx
+++ components/supertabs/index.tsx
@@ -26,60 +26,21 @@ export default function Index(props: TabProps) {
         setValue(newValue);
     };
 
-    const { current} = useRef({
-        isScroll: true, // 是否可以滚动
-        timer: null, // setTimeout句柄
-        delay: 300 // 防抖时间
-    })
-// wheel事件回调
-    const onWheel = useCallback(
-        (e:WheelEvent) => {
-            // 是否可以滚动，不写到setTimeout里面就可以先执行逻辑，在进行防抖，避免造成不跟手的情况
-            if (current.isScroll) {
-                current.isScroll = false;
-                console.log("-----------------------------------"+e.deltaY)
-                handleWheelEvent(e)
-            }
-            // 如果已经上次的防抖效果还没有释放，又触发了wheel回调，那么就取消上次的防抖效果
-            if (current.timer) {
-                clearTimeout(current.timer);
-            }
-            // 重新开始计算
-            current.timer = setTimeout(() => {
-                current.isScroll = true;
-            }, current.delay);
-        },
-        []
-    );
+    
 
-    useEffect(() => {
-        window.addEventListener('wheel', onWheel);
+    function handleLabelWheelEvent(event: WheelEvent) {
+        console.log("======================="+event.deltaY)
+        
 
-        return function () {
-            window.removeEventListener('wheel', onWheel);
-        };
-    }, []);
-
-    function handleWheelEvent(event: WheelEvent) {
-        let index = value;
-        if (event.deltaY > 0) {
-            ++index;
-            if (index > data.length - 1) {
-                index = 0;
-            }
-        } else {
-            --index;
-            if (index < 0) {
-                index = data.length - 1;
-            }
-        }
-        setValue(index);
     }
+    let labelPanel = useRef(); // 这里就是获取ref绑定的那个DOM元素值
 
-    // @ts-ignore
-    // @ts-ignore
+    const divStyle = (): React.CSSProperties => ({
+        
+        
+      });
     return (
-        <div  className="flex h-screen  bg-transparent">
+        <div  className="flex h-screen  bg-transparent" onWheel={handleLabelWheelEvent}>
             <div className="relative flex-none bg-gray-900 opacity-50">
                 <div className="container flex justify-center items-center h-24">
                     <Avatar
@@ -126,7 +87,7 @@ export default function Index(props: TabProps) {
                                 "flex h-full w-full"
                             }
                         >
-                            <div className="grid grid-cols-12 gap-0 mt-64 h-2/3 overflow-y-hidden  bg-green-600">
+                            <div ref={labelPanel} className="grid grid-cols-12 gap-0 mt-64 h-2/3 overflow-y-auto md:no-scrollbar">
                                 {labels.map(({id, name: labelName, url}) => (
                                         <Label key={id} title={labelName} url={url} className={""}></Label>
                                 ))}
